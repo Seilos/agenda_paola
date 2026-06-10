8b159c9a69aa2cbb029572073aaad1f554d7e647
excailus <thexcailus@gmail.com>
2026-06-09 22:11:29 -0400

    cambio en formato de script para pushear

--- subir_a_github.bat
+++ subir_a_github.bat
@@ -2,11 +2,15 @@
 :: Script para subir cambios a GitHub automáticamente
 
 echo Iniciando proceso de subida a GitHub...
+echo.
+echo.
 git add .
+echo.
 echo.
 set /p mensaje="Por favor, escribe el mensaje del commit: "
 git commit -m "%mensaje%"
 git push
 echo.
-echo ¡Listo, Fran! Cambios subidos correctamente al servidor.
+echo.
+echo Cambios subidos correctamente al servidor.
 pause
